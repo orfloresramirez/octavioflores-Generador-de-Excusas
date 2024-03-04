63a3ad999e18fb30643211bfc0c1f609d0b92890
octavio ruben flores ramirez <ruben94fr@hotmail.com>
2024-03-04 19:22:45 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let indexwho = 0;
+  indexwho += Math.floor(Math.random() * who.length);
+  let indexact = 0;
+  indexact += Math.floor(Math.random() * action.length);
+  let indexwhat = 0;
+  indexwhat += Math.floor(Math.random() * what.length);
+  let indexwhen = 0;
+  indexwhen += Math.floor(Math.random() * when.length);
+  let excusa =
+    who[indexwho] +
+    " " +
+    action[indexact] +
+    " " +
+    what[indexwhat] +
+    " " +
+    when[indexwhen];
+
+  document.getElementById("excuse").innerHTML = excusa;
 };
